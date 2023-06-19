7aab54e1cb8a146e4db28bd3ac92d3563e3b9bb7
Giovana Nogueira <114829638+giovananog@users.noreply.github.com>
2023-06-18 22:11:34 -0300

    Update solution.js

--- make-array-consecutive/solution.js
+++ make-array-consecutive/solution.js
@@ -1,1 +1,9 @@
 
+function solution(statues) {
+   let counter = 0;
+   statues.sort((a, b) => a - b);
+   for (let i = 0; i < statues.length; i++){
+       if (statues[i+1] - statues[i] > 1) counter += statues[i+1] - statues[i] - 1;
+   }
+   return counter;
+}
