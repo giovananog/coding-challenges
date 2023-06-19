073e41b434928dfc1290b95efa708055771c102f
Giovana Nogueira <114829638+giovananog@users.noreply.github.com>
2023-06-18 22:14:30 -0300

    Update solution.js

--- almost-increasing-sequence/solution.js
+++ almost-increasing-sequence/solution.js
@@ -1,1 +1,11 @@
+function solution(sequence) {
+  let counter = 0;
+  for (let i = 0; i < sequence.length - 1; i++){
+      if (sequence[i] >= sequence[i+1]) {
+        counter++;
+        if (sequence[i-1] >= sequence[i+1] && sequence[i] >= sequence[i+2] || counter > 1) return false;
+      }   
+  }
+  return true;
+}
 
