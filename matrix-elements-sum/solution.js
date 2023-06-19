7114fcbb09be5c45f7719248f2e0b2bb004b6181
Giovana Nogueira <114829638+giovananog@users.noreply.github.com>
2023-06-18 22:16:53 -0300

    Update solution.js

--- matrix-elements-sum/solution.js
+++ matrix-elements-sum/solution.js
@@ -1,1 +1,14 @@
-
+function solution(matrix) {
+   let sum = 0;
+   for (let i = 0; i < matrix.length; i++){
+       for (let j = 0; j < matrix[i].length; j++){
+           if (matrix[i][j] == 0) {
+               if (i < matrix.length - 1){
+                    matrix[i+1][j] = 0;
+               }
+           }
+               sum += matrix[i][j];
+       }
+   }
+   return sum;
+}
