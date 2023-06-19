323160b6ed2dd00ab0f6e43da2b83293059106bb
Giovana Nogueira <114829638+giovananog@users.noreply.github.com>
2023-06-18 21:58:13 -0300

    Update solution.js

--- palindrome/solution.js
+++ palindrome/solution.js
@@ -1,1 +1,13 @@
-
+function solution(inputString) {
+  
+  let sizeOfInputString = inputString.length;
+  let string2 = "";
+  
+  for (let i = sizeOfInputString-1; i >= 0; i--){
+      string2 += inputString[i];
+  }
+  
+  if (inputString === string2) return true;
+  
+  return false;
+}
