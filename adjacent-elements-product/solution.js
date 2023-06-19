9a769fd954b50b6148cd6687a72eeaab399611b1
Giovana Nogueira <114829638+giovananog@users.noreply.github.com>
2023-06-18 22:02:59 -0300

    Update solution.js

--- adjacent-elements-product/solution.js
+++ adjacent-elements-product/solution.js
@@ -1,1 +1,9 @@
-
+function solution(inputArray) {
+  let largestP = (inputArray[0] * inputArray[1]);
+  for (let i = 1; i < inputArray.length - 1; i++){
+     if ((inputArray[i] * inputArray[i+1]) > largestP){
+         largestP = inputArray[i] * inputArray[i+1];
+     }
+  }
+  return largestP;
+}
